53fc39bba44ad9ac13b4cf194673b5c933d1f2d3
denischornyy <dchornyy@icerockdev.com>
2020-07-17 15:33:23 +0700

    test commit

--- sdk/src/main/java/io/pikassa/sdk/entities/interfaces/IDescription.kt
+++
@@ -1,9 +0,0 @@
-package io.pikassa.sdk.entities.interfaces
-
-/**
-Created by pikassa, support@pikassa.io on 02,Июль,2020
-All rights received.
- */
-interface IDescription {
-    fun getDescription(): String
-}
